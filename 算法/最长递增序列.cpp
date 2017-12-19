62a100d9073b51e5cdf32245c52d208434790382
甘一飞 <30716930+gyhdtc@users.noreply.github.com>
2017-12-20 01:19:04 +0800

    Create 最长递增序列.cpp

---
+++ 算法/最长递增序列.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#includi <vector>
+using namespace std;
+int main() {
+    int x,len=0;
+    vector<int> a;
+    cin>>x;
+    While(x != -1){
+        a.append(x);
+        len++;
+    }
+    int i=1;
+    int start=0;
+    int lent=1;
+    while(i<len){
+        if(a[i]<=a[i-1])start=i;
+        if(lent<i-start+1)lent=i-start+1;
+        i++;
+    }
+    cout<<lent;
+    return 0;
+}
