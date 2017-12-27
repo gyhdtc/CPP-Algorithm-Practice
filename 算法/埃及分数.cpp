6106ade9edf338d264d52cf338a004d38b220a9f
gyhdtc <gyhdtc@hhhsuper.github.com>
2017-12-28 00:28:34 +0800

    1

--- 算法/埃及分数.cpp
+++ 算法/埃及分数.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
-
-int main() {
-    int a,b,i=0;
-    double shang;
-    for(a=23;a<45;a++)
-        for(b=a+1;b<=1100;b++){
-            if((a*b)%45==0){
-                shang=(a*b)/45;
-                if((a+b)/shang==2)i++;
-            }
-        }
-    cout<<i;
-    return 0;
+using namespace std;
+class Solution{
+	public:
+		Solution(int gyh){
+			fenmu=gyh;
+			cout<<"start porject:\n";
+		}
+		int fenmu;
+		int getsolution(){
+			jixianmin=(fenmu%2==0)?fenmu/2:fenmu/2+1;
+			jixianmax=int(jixianmin*fenmu);
+			int i = 0;
+			int a1, a2;
+			int min = jixianmin, max = jixianmax;
+    		double shang;
+    		for(a1 = min; a1 < fenmu; a1++){
+	        	for(a2 = a1+1; a2 <= max; a2++){
+				   if((a1 * a2) % fenmu == 0){
+	      		   		shang=(a1 * a2)/fenmu;
+	       	   		 	if((a1+a2)/shang==2){
+				   			i++;
+				   			cout<<a1<<"   "<<a2<<endl;
+				   		}
+            		}
+        	 	}
+   	 		}
+   	 		return i;
+		}
+	private:
+	int jixianmax;
+	int jixianmin;	
+}; 
+int main(){
+	int problem;
+	cout<<"input your problem you want to solve!"<<endl;
+	cin>>problem;
+    Solution a(problem);
+    int i;
+	i=a.getsolution();
+    cout<<"=== "<<i<<" kinds of soltion to your problem "<<problem<<endl;
+	return 0;
 }
