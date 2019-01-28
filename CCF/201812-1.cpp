65621924ca5f1700cfa8ef9daf71bc398b24fe81
gyhdtc <gyh108wy@163.com>
2019-01-29 01:18:21 +0800

    1

--- CCF/201812-1.cpp
+++ CCF/201812-1.cpp
@@ -1,15 +1,94 @@
 #include <iostream>
 using namespace std;
+int r, y, g;
+
+void gengxin(int a[][2], int j, int t) {
+    if (a[j][1] > t) a[j][1] -= t;
+        
+        else 
+        {    
+            int temp = t;
+            temp = (t - a[j][1]) % (r + y + g);
+
+            switch (a[j][0])
+            {
+                // 1-red 2-yello 3-green
+                // 3-2-1-3-2-1
+                case 1:
+                    if (temp < g) {
+                        a[j][0] = 3;
+                        a[j][1] = g - temp;
+                    }
+                    if (temp < g + y && temp >= g) {
+                        a[j][0] = 2;
+                        a[j][1] = g + y - temp;
+                    }
+                    if (temp < g + y + r && temp >= g + y) {
+                        a[j][0] = 1;
+                        a[j][1] = g + y + r - temp;
+                    }
+                    break;
+                // 1-red 2-yello 3-green
+                // 3-2-1-3-2-1
+                case 2:
+                    if (temp < r) {
+                        a[j][0] = 1;
+                        a[j][1] = r - temp;
+                    }
+                    if (temp < r + g && temp >= r) {
+                        a[j][0] = 3;
+                        a[j][1] = r + g - temp;
+                    }
+                    if (temp < g + y + r && temp >= g + r) {
+                        a[j][0] = 2;
+                        a[j][1] = g + y + r - temp;
+                    }
+                    break;
+                // 1-red 2-yello 3-green                   
+                // 3-2-1-3-2-1
+                case 3:
+                    if (temp < y) {
+                        a[j][0] = 2;
+                        a[j][1] = y - temp;
+                    }
+                    if (temp < r + y && temp >= y) {
+                        a[j][0] = 1;                            
+                        a[j][1] = r + y - temp;
+                    }
+                    if (temp < g + y + r && temp >= y + r) {
+                        a[j][0] = 3;
+                        a[j][1] = g + y + r - temp;
+                    }
+                    break;
+                default:
+                   break;
+            }
+        }
+}
+
 int main() {
-    int r, y, g;
-    cin >> r >> y >> g;
     int i;
+    cin >> r >> y >> g;
     cin >> i;
-    int a, b, t = 0;
-    for (; i > 0; i--) {
-        cin >> a >> b;
-        if (a == 0 || a == 1) t += b;
-        if (a == 2) t += b + r;
+    int a[i][2];
+    int t = 0;
+    for (int j = 0; j < i; j++) {
+        cin >> a[j][0] >> a[j][1];
+    }
+    // 1-red 2-yello 3-green
+    // 3-2-1-3-2-1
+    for (int j = 0; j < i; j++) {
+        if (a[j][0] == 0) t += a[j][1];
+        if (a[j][0] != 0) {
+            gengxin(a, j, t);
+            if (a[j][0] == 1) {
+                t += a[j][1];
+            }
+            if (a[j][0] == 2) {
+                t += a[j][1] + r;
+            }
+        }
     }
     cout << t;
+    return 0;
 }
