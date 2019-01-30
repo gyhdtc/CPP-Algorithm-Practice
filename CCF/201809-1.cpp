53d8056993fe9a1712b105e460586a43c2ef157a
gyhdtc <gyh108wy@163.com>
2019-01-31 00:56:24 +0800

    1

---
+++ CCF/201809-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n], cunzuo;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    cunzuo = a[0];
+    for (int i = 0; i < n-1; i++) {
+        if (i == 0) a[i] = (a[i] + a[i+1]) / 2;
+        else {
+            int t = a[i];
+            a[i] = (cunzuo + a[i] + a[i+1]) / 3;
+            cunzuo = t;
+        }
+    }
+    a[n-1] = (cunzuo + a[n-1]) / 2;
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    return 0;
+}
