ee2fd8acaa5c247297d59cf4712ef45707f624cc
gyhdtc <gyh108wy@163.com>
2019-01-31 00:58:42 +0800

    1

--- CCF/201809-1.cpp
+++ CCF/201809-1.cpp
@@ -7,15 +7,15 @@ int main()
     int a[n], cunzuo;
     for (int i = 0; i < n; i++) cin >> a[i];
     cunzuo = a[0];
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < n; i++) {
         if (i == 0) a[i] = (a[i] + a[i+1]) / 2;
+        else if (i == n-1) a[n-1] = (cunzuo + a[n-1]) / 2;
         else {
             int t = a[i];
             a[i] = (cunzuo + a[i] + a[i+1]) / 3;
             cunzuo = t;
         }
     }
-    a[n-1] = (cunzuo + a[n-1]) / 2;
     for (int i = 0; i < n; i++) cout << a[i] << " ";
     return 0;
 }
