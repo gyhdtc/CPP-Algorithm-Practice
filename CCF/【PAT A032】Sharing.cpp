90510aec1bb15816108474da9ff63e50149dcb04
Your Name <you@example.com>
2019-02-09 21:39:35 +0800

    1

---
+++ CCF/【PAT A032】Sharing.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstring>
+#define maxn 100010
+using namespace std;
+struct Node
+{
+    char data;
+    int next;
+    bool flag;
+}node[maxn];
+int main(){
+    for (int i = 0; i < maxn; i++) node[i].flag = false;
+    int s1, s2, n;
+    scanf("%d%d%d", &s1, &s2, &n);
+    return 0;
+}
+
