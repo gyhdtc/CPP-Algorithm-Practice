21fce705cd3195b711abd7bfdb00bb4ee9e3b7c9
Your Name <you@example.com>
2019-05-05 01:31:31 +0800

    1

--- 随笔/Dijkstra.cpp
+++ 随笔/Dijkstra.cpp
@@ -3,21 +3,15 @@
 using namespace std;
 const int MAXINT = 32767;
 
-struct Node
-{
-    int x;
-    int y;
-};
-
 class Map
 {
     public:
-    int n;// n*n
-    bool s[100];// 是否检测 Ni (i*n+j)点
-    int dist[100];// 走入 Ni 点的代价
-    int prev[100];// 走入 Ni 点要经过的点
-    int A[100][100];// n*n个点，最多10000个
-    int MAX_H;
+        int n;// n*n
+        bool s[100];// 是否检测 Ni (i*n+j)点
+        int dist[100];// 走入 Ni 点的代价
+        int prev[100];// 走入 Ni 点要经过的点
+        int A[100][100];// n*n个点，最多10个
+        int MAX_H;
 
     void print_A(){
         for (int i = 0; i < n*n; i++) {
@@ -50,61 +44,75 @@ class Map
 
 void Creat_A(Map &a) {
     int n = a.n;
-    int x_h[n][n-1];
-    int x_s[n-1][n];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n-1; j++) {
-            cin >> x_h[i][j];
-        }
-    }
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> x_s[i][j];
-        }
-    }
+    int t;
     for (int i = 0; i < n*n; i++) {
         for (int j = 0; j < n*n; j++) {
             a.A[i][j] = MAXINT;
-            if (i == j) a.A[i][j] = 0;
         }
     }
-/*
-    要构造 A 矩阵，我们输入一个n*n矩阵x[n][n]
-    i j 表示 i*n+j 这个点
-    他只会和他的上下左右点连通，
-    连通代价是（要注意判断是否超界）：
-    ← j>0  : A[i*n+j][i*n+j-1] = x[i][j-1]
-    → j<n-1: A[i*n+j][i*n+j+1] = x[i][j+1]
-    ↑ i>0  : A[i*n+j-n][i*n+j] = x[i+1][j]
-    ↓ i<n-1: A[i*n+j+n][i*n+j] = x[i-1][j]
-*/
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n-1; j++) {
-            a.A[i*n+j][i*n+j+1] = x_h[i][j];
-            a.A[i*n+j+1][i*n+j] = x_h[i][j];
+    for (int num = 0; num < n*n; num++) {
+        int i, j;
+        a.A[num][num] = 0;
+        i = num / n;
+        j = num % n;
+        if (i > 0) {
+            a.A[num][num-n] = 1;
+            a.A[num-n][num] = 1;
+        }
+        if (i < n-1) {
+            a.A[num][num+n] = 1;
+            a.A[num+n][num] = 1;
+        }
+        if (j > 0) {
+            a.A[num][num-1] = 1;
+            a.A[num-1][num] = 1;
+        }
+        if (j < n-1) {
+            a.A[num][num+1] = 1;
+            a.A[num+1][num] = 1;
         }
     }
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n; j++) {
-            a.A[i*n+j][i*n+j+n] = x_s[i][j];
-            a.A[i*n+j+1][i*n+j] = x_h[i][j];
+    cin >> t;
+    while(t != -1) {
+        int i, j;
+        i = t / n;
+        j = t % n;
+        if (i > 0) {
+            a.A[t][t-n] = 2*n;
+            a.A[t-n][t] = 2*n;
         }
+        if (i < n-1) {
+            a.A[t][t+n] = 2*n;
+            a.A[t+n][t] = 2*n;
+        }
+        if (j > 0) {
+            a.A[t][t-1] = 2*n;
+            a.A[t-1][t] = 2*n;
+        }
+        if (j < n-1) {
+            a.A[t][t+1] = 2*n;
+            a.A[t+1][t] = 2*n;
+        }
+        cin >> t;
     }
+}
+
+int cha(int a, int b, int n){
+    int cha = abs(a/n - b/n) + abs(a%n - b%n);
+    return 2*cha;
 }
 
 int main(){
     Map gyh;
     int v0, v1, n;
+    
     cin >> gyh.n;
-    gyh.MAX_H = 2 * gyh.n;
-    n = gyh.n;
-/*
-    range : 0 → n*n-1
-    i = v0 / n;
-    j = v0 % n;
-*/
     cin >> v0;
     cin >> v1;
+
+    n = gyh.n;
+    gyh.MAX_H = 2 * n;
+    
     Creat_A(gyh);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -124,19 +132,19 @@ int main(){
         int u = v0;
         // 找出当前未使用的点j的dist[j]最小值
         for(int j = 0; j < n*n; j++) {
-            if((!gyh.s[j]) && gyh.dist[j] < MAXINT) {
+            if((!gyh.s[j]) && gyh.dist[j] + cha(j, v1, n) < mindist) {
                 // u保存当前邻接点中距离最小的点的号码 
                 u = j;
-                mindist = gyh.dist[j];
+                mindist = gyh.dist[j] + cha(j, v1, n);
             }
         }
-        
         gyh.s[u] = true;
-        
+        if (u == v1) break;
+        else cout << u << " ";
         for(int j = 0; j < n*n; j++)
         if((!gyh.s[j]) && gyh.A[u][j] < MAXINT)
         {
-            //在通过新加入的u点路径找到离v0点更短的路径
+            //在通过新加入的u点路径找到离v1点更短的路径
             if(gyh.dist[u] + gyh.A[u][j] < gyh.dist[j])  
             {
                 //更新dist
@@ -146,23 +154,70 @@ int main(){
             }
         }
     }
+    cout << endl;
     while(gyh.prev[v1] != v1) {
         printf("%d <- ", v1);
         v1 = gyh.prev[v1];
     }
+    printf("%d", v1);
     return 0;
 }
 /*
 5
+2
+22
+
+1 1 1 1
+10 10 10 10
+10 10 10 10
+10 10 10 10
+1 1 1 1
+
+1 10 1 10 1
+1 10 1 10 1
+1 10 10 10 1
+1 10 10 10 1
+*/
+
+/*
+5
 0
 20
+
 1 1 1 1
 1 1 1 1
 10 10 10 10
 1 1 1 1
 1 1 1 1
+
 1 1 1 1 1
 10 10 1 10 10
 10 10 1 10 10
 1 1 1 1 1
+*/
+/*
+10
+41
+49
+
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 20 20 20 20 20 1 1
+1 1 1 1 1 20 20 1 1
+1 1 1 1 1 20 20 1 1
+1 1 1 1 1 20 20 1 1
+1 1 20 20 20 20 20 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+
+1 1 1 1 1 1 1 1 1 1
+1 1 1 20 20 20 20 1 1 1
+1 1 1 20 20 20 20 1 1 1
+1 1 1 1 1 1 20 1 1 1
+1 1 1 1 1 1 20 1 1 1
+1 1 1 20 20 20 20 1 1 1
+1 1 1 20 20 20 20 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
 */
