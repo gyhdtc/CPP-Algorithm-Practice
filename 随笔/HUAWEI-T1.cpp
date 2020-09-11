a16f6566e6de616b972cd14162229252257d3598
Your Name <gyh108wy@163.com>
2020-09-11 19:32:47 +0800

    Update HUAWEI-T1.cpp

--- 随笔/HUAWEI-T1.cpp
+++ 随笔/HUAWEI-T1.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main(){
 	unsigned int arr[1000];
 	unsigned int a,index = 0;
+
 	while(cin>>a){
     	arr[index++] = a;
     	char ch = getchar();
     	if(ch == '\n')
-    		break;
+    		break；
     }
+
     unsigned int x = 0X55555555;
     unsigned int y = 0Xaaaaaaaa;
     unsigned int i;
@@ -22,7 +24,6 @@ int main(){
 	}
     
     unsigned int shit = arr[0];
-    
     for (i = 0; i < index-1; i++) {
     	unsigned int b1 = arr[i];
     	unsigned int b2 = arr[i+1];
