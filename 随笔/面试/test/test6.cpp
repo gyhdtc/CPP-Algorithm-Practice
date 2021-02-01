ee8001813030f9288ab3fb2a0181c2d9d257441c
Your Name <gyh108wy@163.com>
2021-02-01 22:04:13 +0800

    1

--- 随笔/面试/test/test6.cpp
+++ 随笔/面试/test/test6.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <memory>
+#include <fstream>
+#include <iterator>
 #include <iostream>
 using namespace std;
 
@@ -29,7 +31,19 @@ int main() {
     // shared_ptr<vector<int>> p = alloca_v();
     // vcin(p);
     // vcout(p);
-    unique_ptr<int> p1(new int(10));
-    fun(p1);
+    // unique_ptr<int> p1(new int(10));
+    // fun(p1);
+    istream_iterator<string> start(cin);
+    istream_iterator<string> eof;
+    vector<string> gyh;
+    while (start != eof) {
+        gyh.push_back(*start++);
+    }
+    cout << gyh.size();
+    ifstream shit("H:/code/C++/Cpp_Primer_Answers/data/books.txt");
+    istream_iterator<string> start2(shit), eof2;
+    vector<string> gyh2;
+    copy(start2, eof2, back_inserter(gyh2));
+    cout << gyh2.size();
     return 0;
 }
