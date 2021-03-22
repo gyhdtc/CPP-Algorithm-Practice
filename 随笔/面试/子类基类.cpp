9fac6048dc20587605b1e46adfb5f553cdc50793
Your Name <gyh108wy@163.com>
2021-03-22 15:51:48 +0800

    1

--- 随笔/面试/子类基类.cpp
+++ 随笔/面试/子类基类.cpp
@@ -4,23 +4,30 @@ using namespace std;
 
 class base {
 public:
+void func() {
+    // func1();
+    cout << 2 << endl;
+}
     virtual void a() {
         cout << "a\n";
     }
 };
 class g : public base {
 public:
+g() {
+    cout << "shit" << endl;
+    func();
+    func1();
+}
+    static void func1() {
+        cout << 1 << endl;
+    }
     virtual void a() {
         cout << "b\n";
     }
 };
 
 int main() {
-    base b;
     g c;
-    base * t = &c;
-    t->a();
-    t = &b;
-    t->a();
     return 0;
 }
