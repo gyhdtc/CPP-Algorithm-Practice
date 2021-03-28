6cc9ec8fc74391c6fd9d0778cca3b5cadf0e04d5
Your Name <gyh108wy@163.com>
2021-03-28 22:46:34 +0800

    1

--- 随笔/面试/3602.cpp
+++ 随笔/面试/3602.cpp
@@ -5,36 +5,24 @@
 using namespace std;
 int res;
 
-vector<int> getjiangluodian(int chushiweizhi, int yicidejuli, vector<int>& jl) {
-    vector<int> t;
-    for (int i = 0; i < jl.size(); ++i) {
-        if (jl[i] > chushiweizhi && jl[i] <= chushiweizhi+yicidejuli) t.push_back(i);
-        if (jl[i] > chushiweizhi+yicidejuli) break;
-    }
-    return t;
-}
-void shit(int sum, int chushiweizhi, int yicidejuli, int cishu, vector<int>& jl, vector<int>& val) {
-    if (cishu == 0) res = max(res, sum);
-    else {
-        vector<int> keyijiangluodian = getjiangluodian(chushiweizhi, yicidejuli, jl);
-        for (int i = 0; i < keyijiangluodian.size(); ++i) {
-            shit(sum+val[keyijiangluodian[i]], jl[keyijiangluodian[i]], yicidejuli, cishu-1, jl, val);
-        }
-    }
+void func(int i) {
+    cout << &i << endl;
+    int a1 = 1;
+    // int a2 = 2;
+    // int a3 = 3;
+    // int a4 = 4;
+    int a5;
+    int a6;
+    int a7;
+    int a8;
+    int a9;
+    int a10;
+    func(i+1);
+    return;
 }
 int main(){
-    res = 0;
-    int n, m, k;
-    cin >> n >> m >> k;
-    vector<int> jl(n);
-    vector<int> val(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> jl[i];
-        cin >> val[i];
-    }
-    vector<int> dp(m*k);
-    int sum = val[0];
-    shit(sum, 0, m, k, jl, val);
-    cout << res << endl;
+    // int a;
+    // cout << sizeof(a);
+    func(0);
     return 0;
 }
