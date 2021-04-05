b2a6e59fcdbdcffa28df68b937d4cc571bb181c6
Your Name <gyh108wy@163.com>
2021-04-05 22:07:50 +0800

    1

--- 随笔/面试/shit.cpp
+++ 随笔/面试/shit.cpp
@@ -1,46 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int compare(vector<int>& a, vector<int>& b) {
-    int res = 0;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] == b[i]) continue;
-        else if (abs(a[i] - b[i]) == 1 ) res++;
-        else return a.size()+1;
-    }
-    return res;
-}
-vector<int> getdengcha(int a, int b, int cha, int n) {
-    vector<int> c(n);
-    for (int i = 0; i < n; i++) c[i] = a - i * cha;
-    return c;
-}
-int main() {
-    int n;
-    cin >> n;
-    int res = n+1;
-    vector<int> a(n);
-    int z[3] = {-1, 0, +1};
-    for (int i = 0; i < n; i++) cin >> a[i];
-    if ((abs(a[n-1] - a[0]) + 2)%(n-1) != 0 && 
-    (abs(a[n-1] - a[0]) - 2)%(n-1) != 0 && 
-    (abs(a[n-1] - a[0]) + 1)%(n-1) != 0 && 
-    (abs(a[n-1] - a[0]) - 1)%(n-1) != 0 && 
-    (abs(a[n-1] - a[0]))%(n-1) != 0) {
-        cout << -1 << endl;
-        return 0;
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            vector<int> c(n);
-            if ((a[0]+z[i] - a[n-1]-z[j])%(n-1) == 0) {
-                c = getdengcha(a[0]+z[i], a[n-1]+z[j], (a[0]+z[i] - a[n-1]-z[j])/(n-1), n);
-                res = min(compare(a, c), res);
+int bianjijuli(string a, string b) {
+    int n = a.size()+1;
+    int m = b.size()+1;
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    dp[0][0] = 0;
+    for (int i = 1; i < n; i++) dp[i][0] = i;
+    for (int j = 1; j < m; j++) dp[0][j] = j;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (a[i-1] == b[j-1]) {
+                dp[i][j] = min(dp[i-1][j-1], min(dp[i][j-1]+1, dp[i-1][j]+1));
+            }
+            else {
+                dp[i][j] = min(dp[i-1][j-1]+1, min(dp[i][j-1]+1, dp[i-1][j]+1));
             }
         }
     }
-    if (res != n+1) cout << res << endl;
-    else cout << -1 << endl;
+    for (auto i : dp) {
+        for (auto j : i) cout << j << " ";
+        cout << endl;
+    }
+    return dp[n-1][m-1];
+}
+
+int main() {
+    string a, b;
+    while (cin >> a >> b) {
+        cout << bianjijuli(b, a) << endl;
+    }
     return 0;
 }
